c1ab87464afa4c514d6ca7bca440d6ed5e1df182
Joyce Liao <joyliao07@gmail.com>
2018-10-16 12:46:02 -0700

    review read-json

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 'use strict';
 
+function Dog(dog) {
+  this.name = dog.name;
+  this.picture = dog.image_url;
+  this.hobbies = dog.hobbies;
+  this.render();
+}
 
 
+Dog.prototype.render = function (){
+  let $sectionNew = $('#dog-template').clone();
+  $('main').append($sectionNew);
+  $sectionNew.find('h2').text(this.name);
+  $sectionNew.find('img').attr('src', this.picture);
+  $sectionNew.find('p').text(this.hobbies);
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Dog.readJson = () => {
+  $.get('data.json', 'json')
+    .then( data => {
+      data.forEach( dog => {
+        new Dog(dog);
+      });
+    });
+};
 
 
 $(document).ready(function() {
-  console.log('j-query is ready');
+  Dog.readJson();
 });
